ad6d48eaa6274c294b659bfd3cc5b79b46583aad
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-01 18:44:29 +0530

    Easy stack but IMP

--- Data Structures/Stack and Queue/Next Greater Element.cpp
+++ Data Structures/Stack and Queue/Next Greater Element.cpp
@@ -44,3 +44,31 @@ Constraints:
 1 ≤ N ≤ 10^6
 1 ≤ Ai ≤ 10^18
 */
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n)
+    {
+        stack<long long> s;
+        vector<long long> ans(n);
+        ans[n-1] = -1;
+        
+        s.push(arr[n-1]);
+        
+        for(int i = n-2; i >= 0; --i)
+        {
+            while(!s.empty() && s.top() <= arr[i])
+                s.pop();
+            
+            if(!s.empty())
+                ans[i] = s.top();
+            else
+                ans[i] = -1;
+            
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+};
