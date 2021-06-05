3593f278c9b6b4d5baa3b6bd90dab327a6d29412
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-05 18:44:47 +0530

    Easy, Variation of LCS

--- DP/Longest Repeating Subsequence.cpp
+++ DP/Longest Repeating Subsequence.cpp
@@ -29,49 +29,24 @@ Constraints:
 1 <= |str| <= 500
 */
 
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
- // } Driver Code Ends
 class Solution {
 	public:
-		int LongestRepeatingSubsequence(string s)
-		{
-		    int n = s.size(), ans = 0;
-		    int dp[n+1][n+1];
-		    memset(dp, 0, sizeof(dp));
+	int LongestRepeatingSubsequence(string s)
+	{
+		int n = s.size();
+		int dp[n+1][n+1];
+		memset(dp, 0, sizeof(dp));
 		    
-		    for(int i = 0; i < n; ++i)
+		for(int i = 1; i <= n; ++i)
+		{
+		    for(int j = 1; j <= n; ++j)
 		    {
-		        for(int j = i+1; j < n; ++j)
-		        {
-		            if(s[i] == s[j])
-		            {
-		                dp[i+1][j+1] = max(dp[i][j]+1, max(dp[i+1][j], dp[i][j+1]));
-		                ans = max(ans, dp[i+1][j+1]);
-		            }
-		            else
-		            {
-		                dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
-		            }
-		        }
-		    }
-		    return ans;
-		}
-
-};
-
-// { Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		string str;
-		cin >> str;
-		Solution obj;
-		int ans = obj.LongestRepeatingSubsequence(str);
-		cout << ans << "\n";
+		        if(i != j && s[i-1] == s[j-1]) // we cant include same char to both subsequences. So i != j
+                    dp[i][j] = dp[i-1][j-1] + 1;
+	            else
+	                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+	        }
+	    }
+	    return dp[n][n];
 	}
-	return 0;
-}  // } Driver Code Ends
+};
