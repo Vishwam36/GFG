3e39be1eeb27f2e84ce70e43ca58ba89ae8436db
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-11 20:04:13 +0530

    IMP

--- Graph/Negative weight cycle.cpp
+++ Graph/Negative weight cycle.cpp
@@ -1,67 +1,37 @@
 // By Vishwam Shriram Mundada
 // https://practice.geeksforgeeks.org/problems/negative-weight-cycle3504/1#
 // Bellman ford implementation
-
-#include<bits/stdc++.h>
-using namespace std;
+// REF : https://www.youtube.com/watch?v=LKfIjVZ6kg4
 
 class Solution {
 public:
 	int isNegativeWeightCycle(int n, vector<vector<int>>edges)
 	{
-	    int m = edges.size();
-	    int dist[n];
-	    for(int i = 0; i < n; ++i)
-	        dist[i] = INT_MAX;
-	    
+	    vector<int> dist(n, INT_MAX);
 	    dist[0] = 0;
 	    
 	    for(int i = 0; i < n-1; ++i)
 	    {
-	        for(int j = 0; j < m; ++j)
+	        for(auto edge : edges)
 	        {
-	            int a = edges[j][0];
-	            int b = edges[j][1];
-	            int w = edges[j][2];
+	            int a = edge[0];
+	            int b = edge[1];
+	            int w = edge[2];
 	            
-	            if(dist[a] != INT_MAX && dist[a] + w < dist[b])
-	            {
-	                dist[b] = dist[a] + w;
-	            }
+	            if(dist[a] != INT_MAX)
+	                dist[b] = min(dist[b], dist[a] + w);
 	        }
 	    }
 	    
-	    for(int i = 0; i < m; ++i)
+	    for(auto edge : edges)
 	    {
-	        int a = edges[i][0];
-	        int b = edges[i][1];
-	        int w = edges[i][2];
+	        int a = edge[0];
+	        int b = edge[1];
+	        int w = edge[2];
 	        
-	        if(dist[a] != INT_MAX && dist[a] + w < dist[b])
-	        {
-	            return 1;
-	        }
+	        if(dist[a] + w < dist[b])
+	            return true;
 	    }
-	    return 0;
+	    return false;
 	}
 };
-
-// { Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>edges;
-		for(int i = 0; i < m; i++){
-			int x, y, z;
-			cin >> x >> y >> z;
-			edges.push_back({x,y,z});
-		}
-		Solution obj;
-		int ans = obj.isNegativeWeightCycle(n, edges);
-		cout << ans <<"\n";
-	}
-	return 0;
-}  // } Driver Code Ends
