c02441b2486095eac12abb4bb56f75689cb734b9
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-15 16:10:10 +0530

    Tricky for N(log N)

--- DP/Longest Increasing Subsequence.cpp
+++ DP/Longest Increasing Subsequence.cpp
@@ -4,36 +4,6 @@
 // decent 
 // https://www.youtube.com/watch?v=qW1O1a40-No
 
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-int longestSubsequence(int, int[]);
-
-int main()
-{
-    //taking total testcases
-    int t,n;
-    cin>>t;
-    while(t--)
-    {
-        //taking size of array
-        cin>>n;
-        int a[n];
-        
-        //inserting elements to the array
-        for(int i=0;i<n;i++)
-            cin>>a[i];
-
-        //calling method longestSubsequence()
-        cout << longestSubsequence(n, a) << endl;
-    }
-}
-// } Driver Code Ends
-
-
-// return length of longest strictly increasing subsequence
-
 int longestSubsequence(int n, int a[])
 {
     vector<int> v;
