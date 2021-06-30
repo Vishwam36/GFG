f6aa58af061d47a297eb0b9647764b3e739570a7
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-30 18:38:07 +0530

    Decent, 2 approaches (trie) + hashing

--- Strings/Print Anagrams Together.cpp
+++ Strings/Print Anagrams Together.cpp
@@ -1,5 +1,6 @@
 // By Vishwam Shriram Mundada
-// 
+// https://practice.geeksforgeeks.org/problems/print-anagrams-together/1#
+// Decent
 
 /*
 Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the original array. 
@@ -38,6 +39,8 @@ Constraints:
 1<=N<=100
 */
 
+// App 1 : Simple approach
+
 vector<vector<string> > Anagrams(vector<string>& string_list) 
 {
     vector<vector<string> > ans;
@@ -59,6 +62,63 @@ vector<vector<string> > Anagrams(vector<string>& string_list)
         for(auto i : indices)
             temp.push_back(string_list[i]);
         
+        ans.push_back(temp);
+    }
+    return ans;
+}
+
+
+// App 2 : Trie approach
+
+struct Node
+{
+    bool isLeaf;
+    Node* children[26];
+};
+
+bool search(Node* root, string s)
+{
+    for(int i = 0; i < s.size(); ++i)
+        if(!root->children[s[i]-'a'])
+            return false;
+    
+    return true;
+}
+
+void insert(Node* root, string s)
+{
+    for(int i = 0; i < s.size(); ++i)
+    {
+        if(!root->children[s[i]-'a'])
+            root->children[s[i]-'a'] = new Node();
+        
+        root = root->children[s[i]-'a'];
+    }
+}
+
+vector<vector<string> > Anagrams(vector<string>& string_list) 
+{
+    Node* root = new Node();
+    map<string, vector<int> > m;
+    for(int i = 0; i < string_list.size(); ++i)
+    {
+        string temp = string_list[i];
+        sort(temp.begin(), temp.end());
+        
+        if(!search(root, string_list[i]))
+            insert(root, string_list[i]);
+        
+        m[temp].push_back(i);
+    }
+    
+    vector<vector<string> > ans;
+    for(auto it = m.begin(); it != m.end(); ++it)
+    {
+        vector<string> temp;
+        for(auto i : it->second)
+        {
+            temp.push_back(string_list[i]);
+        }
         ans.push_back(temp);
     }
     return ans;
